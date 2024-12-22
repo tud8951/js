80c44a0ef8274d8f4a122f7591635d8842671c8e
tud8951 <tud8951@163.com>
2024-12-22 16:02:01 +0800

    new

---
+++ test.py
@@ -0,0 +1,26 @@
+import random
+
+def guess_number():
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+
+    print("欢迎来到猜数字游戏！")
+    print("我已经想好了一个1到100之间的数字。")
+
+    while True:
+        try:
+            guess = int(input("请输入你的猜测: "))
+            attempts += 1
+
+            if guess < number_to_guess:
+                print("太小了！再试一次。")
+            elif guess > number_to_guess:
+                print("太大了！再试一次。")
+            else:
+                print(f"恭喜你，猜对了！你总共猜了 {attempts} 次。")
+                break
+        except ValueError:
+            print("请输入一个有效的数字。")
+
+if __name__ == "__main__":
+    guess_number()
